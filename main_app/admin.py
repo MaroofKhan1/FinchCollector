9536cd5986108f23fbf151d4f7f4c862d690c386
maroofkhan <khan.maroof6569@gmail.com>
2022-05-11 14:09:23 -0500

    WE DONE

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Cat, Feeding
+from .models import Finch, Feeding
 
 # Register your models here.
-admin.site.register(Cat)
+admin.site.register(Finch)
 admin.site.register(Feeding)
 
