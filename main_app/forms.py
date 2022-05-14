411fcc3aeb2a6876ef4cae7d691956b84652e2e1
maroofkhan <khan.maroof6569@gmail.com>
2022-05-13 20:08:42 -0500

    final fix

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,11 +1,8 @@
-from dataclasses import field
-from pyexpat import model
-from statistics import mode
-from django import forms
 from django.forms import ModelForm
 from .models import Feeding
 
 class FeedingForm(ModelForm):
-    class Meta:
-        model = Feeding
-        fields = ['date', 'meal']
+  class Meta:
+    model = Feeding
+    ordering = ['-date']
+    fields = ['date', 'meal']
--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,7 @@ from datetime import date
 MEALS = (
     ('B', 'Breakfast'),
     ('L', 'Lunch'),
-    ('D', 'Dinner')
+    ('D', 'Dinner'),
 )
 
 class Toy(models.Model):
@@ -18,12 +18,14 @@ class Toy(models.Model):
   def get_absolute_url(self):
     return reverse('toys_detail', kwargs={'pk': self.id})
 
+
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
   toys = models.ManyToManyField(Toy)
+
 
   def __str__(self):
     return f'{self.name} ({self.id})'
@@ -32,10 +34,10 @@ class Finch(models.Model):
     return reverse('detail', kwargs={'finch_id': self.id})
 
   def fed_for_today(self):
-    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)  
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS) 
 
 class Feeding(models.Model):
-  date = models.DateField('feeding date')
+  date = models.DateField('Feeding Date')
   meal = models.CharField(
     max_length=1,
     choices=MEALS,
@@ -43,14 +45,12 @@ class Feeding(models.Model):
   )
 
   finch = models.ForeignKey(
-    Finch,
-    on_delete=models.CASCADE
-  )
+        Finch, 
+        on_delete=models.CASCADE
+    )
 
-  class Meta:
+class Meta:
     ordering = ['-date']
 
-  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
-
-  def __str__(self):
+def __str__(self):
     return f"{self.get_meal_display()} on {self.date}"
